a63223d2b92541e76196ed9dc1e0e43f38ff898f
22earth <zhifengle@gmail.com>
2022-08-01 13:45:40 +0800

    fixed: export type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { KvExpiration } from './kv-expiration';
 export { GmEngine } from './gm';
 export { LsEngine } from './local-storage';
 export { JsonEngine } from './json-storage';
-export * from './types';
+export { KvEngine } from './types';
